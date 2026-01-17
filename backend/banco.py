4aef7eb89a4c7acc7c3db0963a905aa6adf0234e
Davi Gauze <davigauze08@gmail.com>
2026-01-16 22:04:23 -0300

    Ajustado a conexão com o banco

--- backend/banco.py
+++ backend/banco.py
@@ -1,14 +1,8 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from datetime import datetime
-from database import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from database import SessionLocal
-from sqlalchemy.orm import Session
-from fastapi import Depends
-
 
 DATABASE_URL = "postgresql://postgres:1205@localhost:5432/postgres"
+Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
@@ -24,22 +18,3 @@ def get_db():
     finally:
         db.close()
 
-Base = declarative_base()
-
-class Usuario(Base):
-    __tablename__ = "usuario"
-    
-    id_usuario = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    senha = Column(String, nullable=False)
-    data_criacao = Column(DateTime, default=datetime.utcnow)
-
-class Imagem(Base):
-    __tablename__ = "imagem"
-    
-    id_imagem = Column(Integer, primary_key=True, index=True)
-    nome_imagem = Column(String, nullable=False)
-    caminho = Column(String, nullable=False)
-    descricao = Column(String, nullable=True)
-    data_upload = Column(DateTime, default=datetime.utcnow)
